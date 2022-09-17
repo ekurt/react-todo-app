67346b739dc0230e82e571844906cacc57ea4eed
Emre Kurt <emrekurtmf@gmail.com>
2022-09-17 12:44:07 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# react-todo-app
+### To Do App with React and Tailwind
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import styles from './App.module.css'
+
+function App() {
+  return (
+    <div className={styles.app}>
+      react-todo-app
+    </div>
+  )
+}
+
+export default App
