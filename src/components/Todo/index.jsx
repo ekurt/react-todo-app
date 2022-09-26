369739236753e5967b70db61f6934736de52367a
Emre Kurt <emrekurtmf@gmail.com>
2022-09-26 20:33:13 +0300

    added flip for todo

--- src/components/Todo/index.jsx
+++ src/components/Todo/index.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import classNames from "classnames";
 import { FaCheckCircle, FaRegCircle, FaTimes } from "react-icons/fa";
+import { BsInfoCircle } from "react-icons/bs";
 import styles from "./index.module.css";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import ReactCardFlip from "react-card-flip";
 
 export const Todo = ({ todo, doneHandle, deleteHandle, notify }) => {
   let todoItem = classNames(styles.todoItem, { [styles.done]: todo.done });
@@ -14,37 +16,68 @@ export const Todo = ({ todo, doneHandle, deleteHandle, notify }) => {
     { [styles.high]: todo.priority === 3 }
   );
 
+  const [flip, setFlip] = useState(false);
+
+  const priorities = ["Normal", "Low", "Medium", "High"];
+
   return (
-    <div className={todoClass}>
-      <svg width="0" height="0">
-        <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
-          <stop stopColor="#2ec0ff" offset="0%" />
-          <stop stopColor="#be82ff" offset="60%" />
-        </linearGradient>
-      </svg>
-      {todo.done ? (
-        <FaCheckCircle
-          size={30}
-          style={{ fill: "url(#gradient)" }}
-          className={styles.check}
-          onClick={() => doneHandle(todo.id)}
+    <ReactCardFlip isFlipped={flip} flipDirection="vertical">
+      <div className={todoClass} onContextMenuCapture={() => setFlip(!flip)}>
+        <svg width="0" height="0">
+          <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+            <stop stopColor="#2ec0ff" offset="0%" />
+            <stop stopColor="#be82ff" offset="60%" />
+          </linearGradient>
+        </svg>
+        {todo.done ? (
+          <FaCheckCircle
+            size={30}
+            style={{ fill: "url(#gradient)" }}
+            className={styles.check}
+            onClick={() => doneHandle(todo.id)}
+          />
+        ) : (
+          <FaRegCircle
+            size={30}
+            style={{ fill: "url(#gradient)" }}
+            className={styles.check}
+            onClick={() => doneHandle(todo.id)}
+          />
+        )}
+
+        <CopyToClipboard text={todo.todo} onCopy={notify}>
+          <span className={todoItem}>{todo.todo}</span>
+        </CopyToClipboard>
+
+        <FaTimes
+          onClick={() => deleteHandle(todo.id)}
+          size={15}
+          className={styles.times}
         />
-      ) : (
-        <FaRegCircle
-          size={30}
-          style={{ fill: "url(#gradient)" }}
-          className={styles.check}
-          onClick={() => doneHandle(todo.id)}
-        />
-      )}
-      <CopyToClipboard text={todo.todo} onCopy={notify}>
-        <span className={todoItem}>{todo.todo}</span>
-      </CopyToClipboard>
-      <FaTimes
-        onClick={() => deleteHandle(todo.id)}
-        size={15}
-        className={styles.times}
-      />
-    </div>
+      </div>
+      <div
+        className={todoClass}
+        onContextMenuCapture={() => setFlip(!flip)}
+        onVolumeChange={() => setFlip(!flip)}
+        onTouchStart={(e) => e.touches.length === 2 ? setFlip(!flip) : false}
+      >
+        <div className={styles.back}>
+          <span className={styles.dateContainer}>
+            <BsInfoCircle
+              size={30}
+              style={{ fill: "url(#gradient)" }}
+              className={styles.info}
+            />
+            Created at: <span className={styles.date}>{todo.date}</span>
+          </span>
+          <span>
+            Priority:
+            <span className={styles[priorities[todo.priority]]}>
+              {priorities[todo.priority]}
+            </span>
+          </span>
+        </div>
+      </div>
+    </ReactCardFlip>
   );
 };
