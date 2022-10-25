a7bc9bdd30462241068629bff22aee9b1209854c
Emre Kurt <emrekurtmf@gmail.com>
2022-10-25 15:13:43 +0300

    added footer links

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import React from "react";
 import styles from "./index.module.css";
 
@@ -5,12 +6,36 @@ export const Footer = () => {
   const priorities = ["normal", "low", "medium", "high"];
 
   return (
-    <div className={styles.footer}>
-      {priorities.map((item, index) => (
-        <div key={index} className={[styles.item, styles[item]].join(' ')}>
-          {item}
+    <>
+      <div className={styles.footer}>
+        {priorities.map((item, index) => (
+          <div key={index} className={[styles.item, styles[item]].join(" ")}>
+            {item}
+          </div>
+        ))}
+      </div>
+      <div className={styles.footerBottom}>
+        <div className={styles.bottomInner}>
+          <span>
+            To contribute, visit my
+            <a
+              href="https://github.com/ekurt/react-cv-maker"
+              target="_blank"
+              className={styles.mark}
+            >
+              github
+            </a>
+            repo.
+          </span>
+          <span>
+            Visit
+            <a href="https://ekurt.dev" target="_blank" className={styles.mark}>
+              my website
+            </a>
+            for my other projects.
+          </span>
         </div>
-      ))}
-    </div>
+      </div>
+    </>
   );
 };
