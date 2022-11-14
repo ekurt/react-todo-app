a8f1fac700ca934f55708610f911c2fc420157ad
Emre Kurt <emrekurtmf@gmail.com>
2022-11-14 13:12:27 +0300

    edited header buttons

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useRef } from "react";
 import classNames from "classnames";
 import styles from "./index.module.css";
-import { FaVolumeUp, FaVolumeMute } from "react-icons/fa";
+import {
+  FaVolumeUp,
+  FaVolumeMute,
+  FaFileDownload,
+  FaFileUpload,
+  FaTrash,
+  FaSortAmountDownAlt,
+  FaSortAmountDown,
+} from "react-icons/fa";
 
 export const Header = ({
   todos,
@@ -9,31 +17,74 @@ export const Header = ({
   deleteAllHandle,
   muted,
   setMuted,
+  sort,
+  importDataHandle,
+  exportDataHandle,
 }) => {
+  const inputFile = useRef(null);
+
+  const handleChange = (e) => {
+    const fileReader = new FileReader();
+    fileReader.readAsText(e.target.files[0], "UTF-8");
+    fileReader.onload = (e) => {
+      importDataHandle(JSON.parse(e.target.result));
+    };
+  };
+
   return (
     <div className={styles.header}>
       <h1 className={styles.h1}>TODO</h1>
       <div className={styles.buttons}>
-        <span className={styles.muted} onClick={() => setMuted(muted === 1 ? 0 : 1)}>
-          {muted === 1 ? <FaVolumeMute size={24} /> : <FaVolumeUp size={24} />}
+        <span
+          className={styles.icon}
+          onClick={() => setMuted(muted === 1 ? 0 : 1)}
+        >
+          {muted === 1 ? <FaVolumeMute size={20} /> : <FaVolumeUp size={20} />}
         </span>
         <span
-          className={classNames(styles.sort, {
+          className={classNames(styles.icon, {
             [styles.disabled]: todos.length === 0,
           })}
           onClick={sortHandle}
         >
-          Sort
+          {sort ? (
+            <FaSortAmountDown size={20} />
+          ) : (
+            <FaSortAmountDownAlt size={20} />
+          )}
         </span>
         <span
-          className={classNames(styles.clear, {
+          className={classNames(styles.icon, {
             [styles.disabled]: todos.length === 0,
           })}
           onClick={deleteAllHandle}
         >
-          Clear All
+          <FaTrash size={20} />
+        </span>
+        <span
+          className={classNames(styles.icon)}
+          onClick={() => {
+            inputFile.current.click();
+          }}
+        >
+          <FaFileUpload size={20} />
+        </span>
+        <span
+          className={classNames(styles.icon, {
+            [styles.disabled]: todos.length === 0,
+          })}
+          onClick={exportDataHandle}
+        >
+          <FaFileDownload size={20} />
         </span>
       </div>
+      <input
+        type="file"
+        id="file"
+        ref={inputFile}
+        onChange={handleChange}
+        style={{ display: "none" }}
+      />
     </div>
   );
 };
