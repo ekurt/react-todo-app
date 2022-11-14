55c52793c8703e9602a5aa479325ea4ce1c0b83c
Emre Kurt <emrekurtmf@gmail.com>
2022-11-14 13:11:35 +0300

    added color in tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,13 @@
-/** @type {import('tailwindcss').Config} */ 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        dark: '#212529'
-      }
+        light: "#AAAAAA",
+        dark: "#212529",
+      },
     },
   },
   plugins: [],
-}
+};
