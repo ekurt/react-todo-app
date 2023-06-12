e2a6af02cadcb905a9b3f5e57dcb5df483e65cd3
Emre Kurt <emrekurtmf@gmail.com>
2023-06-12 15:54:23 +0300

    added pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Todo App",
+        short_name: "Todo",
+        icons: [
+          {
+            src: "/favicon192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/favicon512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        // workbox options here
+      },
+    }),
+  ],
+});
